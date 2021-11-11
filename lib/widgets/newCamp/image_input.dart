05bf6251583aaf298916192583e45e67026bd726
aditya491929 <aditya.malwade@somaiya.edu>
2021-11-11 21:00:48 +0530

    Added ImagePicker on Add New Camp Page

--- lib/widgets/newCamp/image_input.dart
+++ lib/widgets/newCamp/image_input.dart
@@ -1,12 +1,91 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
+  final Function onSelectImage = () => {};
+  // ImageInput(this.onSelectImage);
+
   @override
   _ImageInputState createState() => _ImageInputState();
 }
 
 class _ImageInputState extends State<ImageInput> {
-  var _storedImage = null;
+  File _pickedImage = File('');
+
+  Future<void> _chooseProfilePhoto() async {
+    final picker = ImagePicker();
+    var pickedImageFile;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return new SimpleDialog(
+          title: Text(
+            'Campsite Photo',
+            style: TextStyle(
+              color: Theme.of(context).accentColor,
+              fontSize: 25,
+            ),
+          ),
+          children: [
+            SimpleDialogOption(
+              onPressed: () async {
+                pickedImageFile = await picker.pickImage(
+                  source: ImageSource.camera,
+                );
+                setState(() {
+                  _pickedImage = File(pickedImageFile.path);
+                  Navigator.of(context).pop();
+                });
+                widget.onSelectImage(File(pickedImageFile.path));
+              },
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.camera_alt_outlined,
+                    color: Theme.of(context).accentColor,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Camera',
+                    style: TextStyle(fontSize: 22),
+                  ),
+                ],
+              ),
+            ),
+            SimpleDialogOption(
+              onPressed: () async {
+                pickedImageFile =
+                    await picker.pickImage(source: ImageSource.gallery);
+                setState(() {
+                  _pickedImage = File(pickedImageFile.path);
+                });
+                widget.onSelectImage(File(pickedImageFile.path));
+              },
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.photo_library_outlined,
+                    color: Theme.of(context).accentColor,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Gallery',
+                    style: TextStyle(fontSize: 22),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -21,20 +100,20 @@ class _ImageInputState extends State<ImageInput> {
             ),
             borderRadius: BorderRadius.circular(10),
           ),
-          child: _storedImage != null
-              ? ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.file(
-                    _storedImage,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                  ),
-                )
-              : Text(
+          child: _pickedImage.path.isEmpty
+              ? Text(
                   'Add an image',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
+                  ),
+                )
+              : ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.file(
+                    _pickedImage,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
                   ),
                 ),
           alignment: Alignment.center,
@@ -44,14 +123,13 @@ class _ImageInputState extends State<ImageInput> {
         ),
         Expanded(
           child: TextButton.icon(
-            // onPressed: _takePicture,
-            onPressed: () {},
+            onPressed: _chooseProfilePhoto,
             icon: Icon(
               Icons.camera_alt_outlined,
               color: Colors.amber,
             ),
             label: Text(
-              'Take Picture',
+              'Add Photo',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 17,
