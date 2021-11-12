cd73c931f95230e5dca11773ee4de3ac768a9f85
Vedant Sarnobat <vedant.sarnobat@somaiya.edu>
2021-11-12 21:21:18 +0530

    search bottom modal

--- lib/screens/camp_listings.dart
+++ lib/screens/camp_listings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './camp_detail_screen.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class CampListings extends StatefulWidget {
   @override
@@ -55,6 +56,17 @@ class _CampListingsState extends State<CampListings> {
                     ),
                   ),
                 ),
+                onTap: () {
+                  showModalBottomSheet(
+                    backgroundColor: Color.fromRGBO(48, 48, 48, 1),
+                    barrierColor: Colors.amber,
+                    context: context,
+                    builder: (context) => SingleChildScrollView(
+                      controller: ModalScrollController.of(context),
+                      child: campResults(),
+                    )
+                  );
+                }
               ),
             ),
             SizedBox(
@@ -98,6 +110,44 @@ class _CampListingsState extends State<CampListings> {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget campResults() {
+    return new ListView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: 10,
+      itemBuilder: (context, i) => Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 8,
+        margin:
+            const EdgeInsets.symmetric(horizontal: 5, vertical: 6),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: NetworkImage(
+                  'https://media.istockphoto.com/photos/shot-of-a-cute-vintage-teapot-in-a-campsite-near-to-lake-picture-id1305448692?b=1&k=20&m=1305448692&s=170667a&w=0&h=JIAAnIWgx2dwTi96Zn37rauFCRV11EBIPeTbwAjbpPc='),
+            ),
+            title: Text(
+              'Test ${i + 1}',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              'Location ${i + 1}',
+            ),
+            onTap: () {
+              Navigator.of(context).pushNamed(CampDetail.routeName);
+            },
+          ),
         ),
       ),
     );
