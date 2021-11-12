fc348ca7916b9e98585ed84883c58261df58eba2
Vedant Sarnobat <vedant.sarnobat@somaiya.edu>
2021-11-13 01:25:35 +0530

    query onsearch modal displaying list

--- lib/screens/camp_listings.dart
+++ lib/screens/camp_listings.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import './camp_detail_screen.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import './search_modal.dart';
 
 class CampListings extends StatefulWidget {
   @override
@@ -8,6 +8,38 @@ class CampListings extends StatefulWidget {
 }
 
 class _CampListingsState extends State<CampListings> {
+  // void _search(BuildContext cotext) {
+  //   showModalBottomSheet(
+  //     isScrollControlled: true,
+  //     isDismissible: true,
+  //     shape: RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.vertical(top: Radius.circular(10),),
+  //     ),
+  //     context: context,
+  //     builder: (bCtx) {
+  //       return SearchModal(query: '',);
+  //     },
+  //   );
+  // }
+
+  _renderShowModal(String query){
+    return showModalBottomSheet<void>(
+      isScrollControlled: true,
+      isDismissible: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(10),),
+      ),
+      context: context,
+      builder: (BuildContext context) {
+        // return ValueListenableBuilder(
+        //   // valueListenable: titleController,
+        //   builder: (context, _content, child) {
+            return SearchModal(query: query);
+      //     });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -21,52 +53,66 @@ class _CampListingsState extends State<CampListings> {
         padding: const EdgeInsets.only(left: 5, right: 5, top: 10),
         child: Column(
           children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-              child: TextField(
-                onChanged: (val) {},
-                cursorColor: Colors.white,
-                cursorHeight: 28,
-                textAlignVertical: TextAlignVertical.center,
-                style: TextStyle(
-                  fontSize: 25,
-                ),
-                decoration: InputDecoration(
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                  hintText: 'Search 🔍',
-                  hintStyle: TextStyle(
-                    color: Colors.white70,
+            GestureDetector(
+              // onTap: () {
+              //   _search(context);
+              // },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                // margin:
+                //     const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                width: double.infinity,
+                // decoration: BoxDecoration(
+                //   border: Border.all(width: 2, color: Colors.grey),
+                //   borderRadius: BorderRadius.circular(10),
+                // ),
+                child: TextField(
+                  // onChanged: (val) {},
+                  cursorColor: Colors.white,
+                  cursorHeight: 28,
+                  textAlignVertical: TextAlignVertical.center,
+                  style: TextStyle(
                     fontSize: 25,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Theme.of(context).accentColor,
-                      style: BorderStyle.solid,
+                  decoration: InputDecoration(
+                    contentPadding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    hintText: 'Search 🔍',
+                    hintStyle: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 25,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Theme.of(context).accentColor,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Colors.white,
+                        style: BorderStyle.solid,
+                      ),
                     ),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      width: 1,
-                      color: Colors.white,
-                      style: BorderStyle.solid,
-                    ),
-                  ),
+                  onSubmitted: (query) => {
+                    // print("the query is: " + query),
+                    _renderShowModal(query)
+                    // _search(context)
+                  },
                 ),
-                onTap: () {
-                  showModalBottomSheet(
-                    backgroundColor: Color.fromRGBO(48, 48, 48, 1),
-                    barrierColor: Colors.amber,
-                    context: context,
-                    builder: (context) => SingleChildScrollView(
-                      controller: ModalScrollController.of(context),
-                      child: campResults(),
-                    )
-                  );
-                }
+                // child: Text(
+                //   'Search 🔍',
+                //   style: TextStyle(
+                //     color: Colors.white70,
+                //     fontSize: 25,
+                //   ),
+                // ),
               ),
             ),
             SizedBox(
@@ -110,44 +156,6 @@ class _CampListingsState extends State<CampListings> {
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget campResults() {
-    return new ListView.builder(
-      physics: BouncingScrollPhysics(),
-      itemCount: 10,
-      itemBuilder: (context, i) => Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        elevation: 8,
-        margin:
-            const EdgeInsets.symmetric(horizontal: 5, vertical: 6),
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: ListTile(
-            leading: CircleAvatar(
-              radius: 30,
-              backgroundImage: NetworkImage(
-                  'https://media.istockphoto.com/photos/shot-of-a-cute-vintage-teapot-in-a-campsite-near-to-lake-picture-id1305448692?b=1&k=20&m=1305448692&s=170667a&w=0&h=JIAAnIWgx2dwTi96Zn37rauFCRV11EBIPeTbwAjbpPc='),
-            ),
-            title: Text(
-              'Test ${i + 1}',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: Text(
-              'Location ${i + 1}',
-            ),
-            onTap: () {
-              Navigator.of(context).pushNamed(CampDetail.routeName);
-            },
-          ),
         ),
       ),
     );
---
+++ lib/screens/search_modal.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class SearchModal extends StatefulWidget {
+
+  final String query;
+
+  SearchModal({required this.query});
+
+  @override
+  _SearchModalState createState() => _SearchModalState();
+}
+
+class _SearchModalState extends State<SearchModal> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        padding: const EdgeInsets.only(top: 10, left: 5, right: 5),
+        height: MediaQuery.of(context).size.height * 0.65,
+        // child: Column(
+        //   children: [
+        //     Container(
+        //       height: 5,
+        //       width: 55,
+        //       decoration: BoxDecoration(
+        //         color: Theme.of(context).accentColor,
+        //         borderRadius: BorderRadius.circular(10)
+        //       ),
+              
+        //     ),
+        //   ],
+        // ),
+        child: ListView.builder(
+          physics: BouncingScrollPhysics(),
+          itemCount: 10,
+          itemBuilder: (context, i) => Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 8,
+            margin:
+                const EdgeInsets.symmetric(horizontal: 5, vertical: 6),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(
+                      'https://media.istockphoto.com/photos/shot-of-a-cute-vintage-teapot-in-a-campsite-near-to-lake-picture-id1305448692?b=1&k=20&m=1305448692&s=170667a&w=0&h=JIAAnIWgx2dwTi96Zn37rauFCRV11EBIPeTbwAjbpPc='),
+                ),
+                title: Text(
+                  'Test ${i + 1}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  'Location ${i + 1}',
+                ),
+                onTap: () {print('query is: ' + widget.query);},
+              ),
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
