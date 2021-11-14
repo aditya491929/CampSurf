b996d91d99ee362e522d304a8168b57abaad9d72
Vedant Sarnobat <vedant.sarnobat@somaiya.edu>
2021-11-14 19:18:03 +0530

    get data from db

--- lib/screens/camp_listings.dart
+++ lib/screens/camp_listings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './camp_detail_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CampListings extends StatefulWidget {
   @override
@@ -8,8 +9,16 @@ class CampListings extends StatefulWidget {
 
 class _CampListingsState extends State<CampListings> {
 
+  Future<void> _refreshCamps(BuildContext context) async {
+    setState(() {});
+  }
+
+  
   @override
   Widget build(BuildContext context) {
+
+    CollectionReference camps = FirebaseFirestore.instance.collection('camp-details');
+    
     return GestureDetector(
       onDoubleTap: () {
         final FocusScopeNode currentScope = FocusScope.of(context);
@@ -21,66 +30,74 @@ class _CampListingsState extends State<CampListings> {
         padding: const EdgeInsets.only(left: 5, right: 5, top: 10),
         child: Column(
           children: [
-            // GestureDetector(
-            //   onTap: () {
-            //     _renderShowModal();
-            //   },
-            //   child: Container(
-            //     padding:
-            //         const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-            //     margin:
-            //         const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-            //     width: double.infinity,
-            //     decoration: BoxDecoration(
-            //       border: Border.all(width: 2, color: Colors.grey),
-            //       borderRadius: BorderRadius.circular(10),
-            //     ),
-            //     child: Text(
-            //       'Search 🔍',
-            //       style: TextStyle(
-            //         color: Colors.white70,
-            //         fontSize: 25,
-            //       ),
-            //     ),
-            //   ),
-            // ),
             SizedBox(
               height: 5,
             ),
             Expanded(
-              child: ListView.builder(
-                physics: BouncingScrollPhysics(),
-                itemCount: 10,
-                itemBuilder: (context, i) => Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  elevation: 8,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 5, vertical: 6),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: NetworkImage(
-                            'https://media.istockphoto.com/photos/shot-of-a-cute-vintage-teapot-in-a-campsite-near-to-lake-picture-id1305448692?b=1&k=20&m=1305448692&s=170667a&w=0&h=JIAAnIWgx2dwTi96Zn37rauFCRV11EBIPeTbwAjbpPc='),
-                      ),
-                      title: Text(
-                        'Test ${i + 1}',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+              child: RefreshIndicator(
+                onRefresh: () => _refreshCamps(context),
+                color: Theme.of(context).accentColor,
+                child: FutureBuilder(
+                  future: camps.orderBy('cid', descending: true).get(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          color: Theme.of(context).accentColor,
                         ),
-                      ),
-                      subtitle: Text(
-                        'Location ${i + 1}',
-                      ),
-                      onTap: () {
-                        Navigator.of(context).pushNamed(CampDetail.routeName);
-                      },
-                    ),
-                  ),
+                      );
+                    }
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return Scrollbar(
+                        child: ListView.builder(
+                          physics: BouncingScrollPhysics(),
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, i) {
+                            final campDoc = snapshot.data!.docs[i];
+                            final address1 = campDoc['address'].split(',');
+                            return Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              elevation: 8,
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 5, vertical: 6),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: ListTile(
+                                  leading: CircleAvatar(
+                                    radius: 30,
+                                    backgroundImage: NetworkImage(
+                                      campDoc['image_url'],
+                                    ),
+                                  ),
+                                  title: Text(
+                                    campDoc['title'],
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  subtitle: Text(
+                                    '${address1[3]}, ${address1[4]}',
+                                    overflow: TextOverflow.fade,
+                                    maxLines: 1,
+                                    softWrap: false,
+                                  ),
+                                  onTap: () {
+                                    Navigator.of(context)
+                                        .pushNamed(CampDetail.routeName);
+                                  },
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }
+                    return Center(child: Text('No Camps 🙁!'));
+                  },
                 ),
               ),
             ),
